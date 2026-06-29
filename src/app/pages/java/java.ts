53c6c0dcddac66c3671457939797fdec3c7d3f30
vikashkarma <vikashkarma75@gmail.com>
2026-06-29 17:29:56 +0530

    Added default SEO meta tags

--- src/app/pages/java/java.ts
+++ src/app/pages/java/java.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ARTICLES } from '../../data/articles';
 import { CommonModule } from '@angular/common';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-java',
@@ -11,6 +12,22 @@ import { CommonModule } from '@angular/common';
 })
 export class Java {
 
+  constructor(
+  private title: Title,
+  private meta: Meta
+) {}
+
+ngOnInit() {
+
+  this.title.setTitle('Java Tutorials & Interview Questions | TechSphere');
+
+  this.meta.updateTag({
+    name: 'description',
+    content: 'Java tutorials, interview questions, OOP concepts and programming articles.'
+  });
+
+}
+   
   javaArticles = ARTICLES.filter(
   item => item.category === 'Java'
 
